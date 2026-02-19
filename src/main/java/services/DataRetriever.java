7fc08b5a7542f112bf9ee6d7b6a46473b3615477
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-19 07:17:42 +0300

    feat(find_Confirmed_And_Paid_Invoice_Totals): implement the method findConfirmedAndPaidInvoiceTotals() int the DataRetriever class

--- src/main/java/services/DataRetriever.java
+++ src/main/java/services/DataRetriever.java
@@ -54,4 +54,39 @@ public class DataRetriever {
         }
     }
 
+    public List<InvoiceTotal> findConfirmedAndPaidInvoiceTotals() {
+        String sql = """
+        SELECT i.id, i.customer_name, i.status,
+               SUM(il.quantity * il.unit_price) AS total_amount
+        FROM invoice i
+        JOIN invoice_line il ON i.id = il.invoice_id
+        WHERE i.status IN ('CONFIRMED', 'PAID')
+        GROUP BY i.id, i.customer_name, i.status
+        ORDER BY i.id;
+    """;
+
+        Connection connection = dbConnection.getDBConnection();
+        List<InvoiceTotal> results = new ArrayList<>();
+
+        try (PreparedStatement ps = connection.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("customer_name");
+                InvoiceStatus status = InvoiceStatus.valueOf(rs.getString("status"));
+                BigDecimal total = rs.getBigDecimal("total_amount");
+
+                results.add(new InvoiceTotal(id, name, status, total));
+            }
+
+            return results;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            dbConnection.close(connection);
+        }
+    }
+
 }
